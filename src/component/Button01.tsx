ef7ea250d8e90a3ee40d40d53be338ed2558ff54
Fable01 <phuriwat.preedee.gmail.com>
2025-11-13 17:10:10 +0700

    commit

--- src/component/Button01.tsx
+++ src/component/Button01.tsx
@@ -1,58 +1,71 @@
 import React from 'react'
+import { ArrowRight } from 'lucide-react';
+
+
 
 const Button01 = () => {
     return (<>
         <div className='   
          group
         flex w-[300px] h-[100px] bg-amber-200 justify-center items-center
-        border-4 border-black transition-all duration-300
+        border-3 border-black transition-all duration-300
         hover:translate-x-[4px] 
         hover:-translate-y-[4px]
         hover:shadow-[-12px_12px_0px_black]
-         
-       
-      "
     '>
-            <div>
-                <span className='
+            <div  className=' flex '>
+                <div className='
                 overflow-hidden
                 relative
                  font-bold  text-[24px] 
                  text-center flex justify-between 
                  items-center   w-[200px] h-[40px] '>
-
-                    <div
-                        className="
+                <div className="
             absolute inset-0  flex items-center justify-center
              duration-300 ease-in-out
             group-hover:-translate-y-full
-          "
-                    >
-                        Hover Me
-                    </div>
+          ">Hover Me</div>
 
-
-
-                    <div
-                        className="
+                <div className="
             absolute inset-0 flex items-center justify-center
             duration-300 ease-in-out
             translate-y-[30px] group-hover:translate-y-0
-          "
-                    >
-                        Hover Me
-                    </div>
+          ">Hover Me</div>
+
+                </div>
+
+                <div className='
+               overflow-hidden
+                relative
+                 font-bold  text-[24px] 
+                 text-center flex justify-between 
+                 items-center   w-[40px] h-[40px] '>
+                <div className="
+            absolute inset-0  flex items-center justify-center
+             duration-300 ease-in-out
+            group-hover:-translate-x-[-50px]
+          "><ArrowRight/></div>
+
+                <div className="
+                text-red-500
+            absolute inset-0 flex items-center justify-center
+            duration-300 ease-out
+            translate-x-[-60px] group-hover:translate-x-0
+          "><ArrowRight/></div>
 
 
-                </span>
+
+
+
+                </div>
+
+
+
 
 
             </div>
 
         </div>
-
-
-
 
     </>
 
